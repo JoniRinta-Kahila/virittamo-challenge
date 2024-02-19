bc50b6e0925b74d6d544c22d532f16a5aeb14e48
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-19 19:06:56 +0200

    Init router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Layout from './components/layout'
+import { Route, Routes } from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
     <Layout>
-      VIRITTÄMÖ HELSINKI
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/devices" element={<div>Devices</div>} />
+        <Route path="/issuance" element={<div>Device issuance</div>} />
+        <Route path="/devices/:deviceId" element={<div>Device details</div>} />
+        <Route path="/settings" element={<div>Settings</div>} />
+        <Route path='*' element={<div>Not found</div>} />
+      </Routes>
     </Layout>
   )
 }
--- front/src/components/sidebar.tsx
+++ front/src/components/sidebar.tsx
@@ -7,70 +7,71 @@ import {
 } from '@heroicons/react/24/outline'
 import logo from '../assets/logo.jpg';
 import classNames from 'classnames'
+import { Link } from 'react-router-dom';
 
 interface SidebarProps {
 
 }
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Devices', href: '#', icon: FolderIcon, current: false },
-  { name: 'Device issuance', href: '#', icon: FolderArrowDownIcon, current: false },
+  { name: 'Dashboard', href: '/', icon: HomeIcon, current: true },
+  { name: 'Devices', href: '/devices', icon: FolderIcon, current: false },
+  { name: 'Device issuance', href: '/issuance', icon: FolderArrowDownIcon, current: false },
 ]
 
 const Sidebar: React.FC<SidebarProps> = () => {
   return (
     <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-            <div className="flex h-24 shrink-0 items-center">
-              <img
-                className="h-16 w-auto"
-                src={logo}
-                alt="Virittämö Helsinki"
-              />
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-50 text-indigo-600'
-                              : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
-                              'h-6 w-6 shrink-0'
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="mt-auto">
-                  <a
-                    href="#"
-                    className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+      <div className="flex h-24 shrink-0 items-center">
+        <img
+          className="h-16 w-auto"
+          src={logo}
+          alt="Virittämö Helsinki"
+        />
+      </div>
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="-mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    to={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-50 text-indigo-600'
+                        : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                    )}
                   >
-                    <Cog6ToothIcon
-                      className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
+                    <item.icon
+                      className={classNames(
+                        item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
+                        'h-6 w-6 shrink-0'
+                      )}
                       aria-hidden="true"
                     />
-                    Settings
-                  </a>
+                    {item.name}
+                  </Link>
                 </li>
-              </ul>
-            </nav>
-          </div>
+              ))}
+            </ul>
+          </li>
+          <li className="mt-auto">
+            <Link
+              to="/settings"
+              className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+            >
+              <Cog6ToothIcon
+                className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
+                aria-hidden="true"
+              />
+              Settings
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
