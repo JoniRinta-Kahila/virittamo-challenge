fbb4c73731d64ce2d2c249547dbd629a992f1d8b
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-19 18:23:05 +0200

    Init front. React + Tailwind

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <h1 className='font-bold border-b-2'> 🏗️ Virittämö haaste 🚧</h1>
+    </div>
+  )
+}
+
+export default App
