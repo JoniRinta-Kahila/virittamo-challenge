1b35ed7a2d80923a994b949539bd66f370ac300b
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-20 14:59:47 +0200

    Update Issuance form

--- front/src/apiUtils.ts
+++ front/src/apiUtils.ts
@@ -1,10 +1,38 @@
-import { DeviceDocument } from "./types";
+import { DeviceDocument, DevicesResponse } from "./types";
+import { DeviceIssuanceInfo } from "../../shared/types/issuanceInfo";
+
+const baseUrl = `${window.location.protocol}//${window.location.hostname}:3001/api`;
 
 const getDeviceById = async (id: string): Promise<DeviceDocument> => {
   const response = await fetch(`${window.location.protocol}//${window.location.hostname}:3001/api/devices/${id}`);
   return await response.json();
 }
 
+const getIssuancesByDeviceId = async (deviceId: string) => {
+  const response = await fetch(`${baseUrl}/issuances/device/${deviceId}`);
+  console.log("getIssuancesByDeviceId", response);
+  return await response.json();
+}
+
+const searchDevicesByName = async (name: string): Promise<DevicesResponse> => {
+  const response = await fetch(`${baseUrl}/devices/search/${name}`);
+  return await response.json();
+}
+
+const createIssuance = async (issuanceInfo: DeviceIssuanceInfo) => {
+  const response = await fetch(`${baseUrl}/issuances`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(issuanceInfo)
+  });
+  return await response.json();
+}
+
 export default {
   getDeviceById,
+  getIssuancesByDeviceId,
+  searchDevicesByName,
+  createIssuance,
 }
--- front/src/components/issuanceForm.tsx
+++ front/src/components/issuanceForm.tsx
@@ -1,11 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DatePicker from "react-datepicker";
+import apiUtils from '../apiUtils';
 
 const IssuanceForm: React.FC = () => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [dueDate, setDueDate] = useState<Date | null>(null);
+
+  const [deviceName, setDeviceName] = useState<string>('');
+  const [suggestedValues, setSuggestedValues] = useState<string[]>([]);
+
+  const [manufacturers, setManufacturers] = useState<string[]>([]);
+  const [deviceNumbers, setDeviceNumbers] = useState<string[]>([]);
+
+  const { searchDevicesByName, createIssuance } = apiUtils;
+
+  useEffect(() => {
+    if (!deviceName) {
+      setSuggestedValues([])
+      setManufacturers([])
+      setDeviceNumbers([])
+      return;
+    }
+
+    try {
+      searchDevicesByName(deviceName)
+        .then((data) => {
+          const suggestedManufacturers = Array.from(new Set(data.results.map((device) => device.manufacturer)))
+          const suggestedDeviceNumbers = Array.from(new Set(data.results.map((device) => device.deviceNumber)))
+
+          setManufacturers(suggestedManufacturers)
+          setDeviceNumbers(suggestedDeviceNumbers)
+          setSuggestedValues(data.results.map((device) => device.name))
+        })
+    } catch (error) {
+      //
+    }
+  }, [deviceName, searchDevicesByName])
+
+  const handleSelect = (e: React.MouseEvent<HTMLLIElement>) => {
+    setDeviceName(e.currentTarget.textContent || '');
+    setSuggestedValues([]);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const device = {
+      name: deviceName,
+      manufacturer: e.currentTarget['manufacturer'].value as string,
+      deviceNumber: e.currentTarget['device-number'].value as string,
+    }
+
+    const recipient = {
+      name: e.currentTarget['recipient-name'].value,
+      department: e.currentTarget['recipient-department'].value
+    }
+
+    const issuance = {
+      issuedAt: startDate,
+      dueDate
+    }
+
+    createIssuance({
+      deviceDetails: device,
+      recipientDetails: recipient,
+      dateOfIssue: issuance.issuedAt!.toISOString(),
+      returningDate: issuance.dueDate!.toISOString()
+    })
+    .then((data) => console.log(data))
+    .then(() => {})
+    .catch((error) => console.error(error))
+  }
+
   return (
-    <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+    <form onSubmit={handleSubmit} className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
       <div className="px-4 py-6 sm:p-8">
         <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <h2 className="text-lg font-semibold leading-6 text-gray-900 col-span-full">Device details</h2>
@@ -18,10 +85,17 @@ const IssuanceForm: React.FC = () => {
                 type="text"
                 name="device-name"
                 id="device-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={deviceName}
+                onChange={(e) => setDeviceName(e.target.value)}
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 required
               />
             </div>
+            {!!suggestedValues.length && <ul className='bg-slate-50 p-2 border-2 border-t-0'>
+              {suggestedValues.map((value) => (
+                <li onClick={handleSelect} className='hover:bg-white cursor-pointer font-semibold' key={value}>{value}</li>
+              ))}
+            </ul>}
           </div>
 
           <div className="sm:col-span-3">
@@ -29,13 +103,19 @@ const IssuanceForm: React.FC = () => {
               Manufacturer
             </label>
             <div className="mt-2">
-              <input
-                type="text"
+              <select
                 name="manufacturer"
                 id="manufacturer"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                disabled={manufacturers.length === 0}
+                defaultValue={manufacturers.length > 1 ? '' : manufacturers[0]}
                 required
-              />
+              >
+                {manufacturers.length > 1 && <option value="" disabled selected>Select a manufacturer</option>}
+                {manufacturers.map((manufacturer) => (
+                  <option key={manufacturer} value={manufacturer}>{manufacturer}</option>
+                ))}
+              </select>
             </div>
           </div>
 
@@ -44,13 +124,19 @@ const IssuanceForm: React.FC = () => {
               Device number
             </label>
             <div className="mt-2">
-              <input
-                id="device-number"
+              <select
                 name="device-number"
-                type="device-number"
+                id="device-number"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                disabled={deviceNumbers.length === 0}
+                defaultValue={deviceNumbers.length > 1 ? '' : deviceNumbers[0]}
                 required
-              />
+              >
+                {deviceNumbers.length > 1 && <option value="" disabled selected>Select a device number</option>}
+                {deviceNumbers.map((deviceNumber) => (
+                  <option key={deviceNumber} value={deviceNumber}>{deviceNumber}</option>
+                ))}
+              </select>
             </div>
           </div>
 
@@ -64,7 +150,7 @@ const IssuanceForm: React.FC = () => {
                 type="text"
                 name="recipient-name"
                 id="recipient-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 required
               />
             </div>
@@ -79,7 +165,7 @@ const IssuanceForm: React.FC = () => {
                 type="text"
                 name="recipient-department"
                 id="recipient-department"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 required
               />
             </div>
