3f1e6410a81a0f3172e283e868c67d3d5315132b
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-20 17:00:47 +0200

    Setup vite proxy for api

--- front/src/apiUtils.ts
+++ front/src/apiUtils.ts
@@ -1,38 +1,36 @@
 import { DeviceDocument, DevicesResponse } from "./types";
 import { DeviceIssuanceInfo } from "../../shared/types/issuanceInfo";
 
-const baseUrl = `${window.location.protocol}//${window.location.hostname}:3001/api`;
-
 const getDeviceById = async (id: string): Promise<DeviceDocument> => {
-  const response = await fetch(`${window.location.protocol}//${window.location.hostname}:3001/api/devices/${id}`);
+  const response = await fetch(`/api/devices/${id}`);
   return await response.json();
 }
 
 const getDevicesByPage = async (page: number): Promise<DevicesResponse> => {
-  const response = await fetch(`${baseUrl}/devices?page=${page}`);
+  const response = await fetch(`/api/devices?page=${page}`);
   return await response.json();
 }
 
 const getIssuancesByDeviceId = async (deviceId: string) => {
-  const response = await fetch(`${baseUrl}/issuances/device/${deviceId}`);
+  const response = await fetch(`/api/issuances/device/${deviceId}`);
   console.log("getIssuancesByDeviceId", response);
   return await response.json();
 }
 
 const searchDevicesByName = async (name: string): Promise<DevicesResponse> => {
-  const response = await fetch(`${baseUrl}/devices/search/${name}`);
+  const response = await fetch(`/api/devices/search/${name}`);
   return await response.json();
 }
 
 const deleteDeviceById = async (id: string) => {
-  const response = await fetch(`${baseUrl}/devices/${id}`, {
+  const response = await fetch(`/api/devices/${id}`, {
     method: 'DELETE'
   });
   return await response.json();
 }
 
 const createIssuance = async (issuanceInfo: DeviceIssuanceInfo) => {
-  const response = await fetch(`${baseUrl}/issuances`, {
+  const response = await fetch(`/api/issuances`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
