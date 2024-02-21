91d27cca6332b3496566041a3996f1e4759b173e
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-21 15:40:08 +0200

    Decompose component into smaller, reusable parts

--- front/src/components/devicesList.tsx
+++ front/src/components/devicesList.tsx
@@ -1,10 +1,8 @@
-import React, { Fragment, useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Menu, Transition } from '@headlessui/react'
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import classNames from 'classnames';
 import { DeviceDocument } from '../types';
 import apiUtils from '../apiUtils';
+import DeviceContextMenu from './deviceContextMenu';
 
 const DevicesList: React.FC = () => {
   const [devices, setDevices] = React.useState<DeviceDocument[]>([]);
@@ -43,7 +41,7 @@ const DevicesList: React.FC = () => {
                 <p
                   className="rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset"
                 >
-                  DEVICE STATUS HERE
+                  STATUS
                 </p>
               </div>
               <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
@@ -53,7 +51,7 @@ const DevicesList: React.FC = () => {
                 <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
                   <circle cx={1} cy={1} r={1} />
                 </svg>
-                <p className="truncate">asdasd</p>
+                <p className="truncate">{device.deviceNumber}</p>
               </div>
             </div>
             <div className="flex flex-none items-center gap-x-4">
@@ -63,63 +61,7 @@ const DevicesList: React.FC = () => {
               >
                 View details<span className="sr-only"> Device details</span>
               </Link>
-              <Menu as="div" className="relative flex-none">
-                <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                  <span className="sr-only">Open options</span>
-                  <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
-                </Menu.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to={`/issuance/${device._id}`}
-                          className={classNames(
-                            active ? 'bg-gray-50' : '',
-                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                          )}
-                        >
-                          Issuance<span className="sr-only">, {device.name}</span>
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to={`/devices/device?id=${device._id}`}
-                          className={classNames(
-                            active ? 'bg-gray-50' : '',
-                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                          )}
-                        >
-                          Edit<span className="sr-only">, {device.name}</span>
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          onClick={() => removeDevice(device._id)}
-                          className={classNames(
-                            active ? 'bg-gray-50' : '',
-                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                          )}
-                        >
-                          Delete<span className="sr-only">, {device.name}</span>
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+              <DeviceContextMenu deviceDoc={device} removeDeviceFn={removeDevice} />
             </div>
           </li>
         ))}
