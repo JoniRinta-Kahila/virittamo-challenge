3381b10a57cab93c6f8289a559b9318db08e2100
Joni Rinta-Kahila <joni.rinta.kahila@gmail.com>
2024-02-21 15:38:38 +0200

    Update cancel button

--- front/src/components/addDeviceForm.tsx
+++ front/src/components/addDeviceForm.tsx
@@ -79,7 +79,7 @@ const AddUpdateDeviceForm: React.FC<AddDeviceFormProps> = ({ deviceId }) => {
         </div>
       </div>
       <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+        <button type="button" onClick={() => navigate("/devices")} className="text-sm font-semibold leading-6 text-gray-900">
           Cancel
         </button>
         <button
